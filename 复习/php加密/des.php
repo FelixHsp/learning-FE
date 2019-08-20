1f9637d2fb35397a16daacfb5f6f6ab00e924d19
Felix <13115412392@163.com>
2019-08-20 10:05:51 +0800

    php加密des

--- 复习/php加密/des.php
+++ 复习/php加密/des.php
@@ -1,1 +1,12 @@
 <?php
+//$a = openssl_get_cipher_methods();
+//print_r($a);
+
+$data = 13115412392;
+$key = uniqid();
+var_dump($key);
+$method = "des-ede3";
+$content = openssl_encrypt($data,$method,$key,OPENSSL_RAW_DATA);
+echo($content);
+$content2=openssl_decrypt($content,$method,$key,OPENSSL_RAW_DATA);
+echo ($content2);
