4c91b4d89a3e906893b0ab45ad155584aaa1b326
Felix <13115412392@163.com>
2019-09-06 19:51:01 +0800

    0

--- 复习/promise/kPromise.js
+++ 复习/promise/kPromise.js
@@ -7,16 +7,18 @@ class kPromise {
         this.resolvedList = [];
         this.rejectedList = [];
         this.status = kPromise.PENDING;
+        this.value;
         handler ( this._resolve.bind(this) , this._reject.bind(this) );//如果传的是函数，就执行。
     }
-    _resolve() {
+    _resolve(val) {
         // console.log('resolve')
         window.addEventListener( 'message' , () => {
             if(this.status !== kPromise.PENDING ) return;//防止状态二次改变
             this.status = kPromise.RESOLVED; //正常resolve直接调用，this指向window，需在handler里bind一下
+            this.value = val;
             let handler;
             while ( handler = this.resolvedList.shift() ) {
-                handler();
+                handler(this.value);
             }
         })
         window.postMessage('');
@@ -36,6 +38,9 @@ class kPromise {
         // resolvedHandler();
         this.resolvedList.push(resolvedHandler);
         this.rejectedList.push(rejectedHandeler);
+        return new kPromise ( (resolve , rejecte) => {
+            resolve();
+        })
         
     }
 }
