54d9405e426ba52cc19c5fe0c5bc40d0f5f6c205
Felix <13115412392@163.com>
2019-09-15 20:02:23 +0800

    0

--- 复习/数据结构与算法/1.js
+++ 复习/数据结构与算法/1.js
@@ -1,7 +1,8 @@
 let arr = [1,2,3,4,5,6,7,8,9,10];
 let sum = 6;
 
-function add( arr , sum ){
+//两次循环
+/* function add( arr , sum ){
     for(let i = 0 ;i < arr.length ;i++){
         for(let j = i ; j < arr.length ;j++){
             if(arr[i]+arr[j] == sum && arr[i] != arr[j]){
@@ -9,6 +10,16 @@ function add( arr , sum ){
             }
         }
     }
-}
+} */
 
-add (arr , sum)
+//一次循环
+function add(arr,sum){
+    let obj = {}
+    arr.forEach( (v,i) => {
+        if(String (v) in obj ){
+            console.log('找到了')
+            console.log(obj[v] , i)
+        }
+        obj[sum - v] = i
+    });
+}
