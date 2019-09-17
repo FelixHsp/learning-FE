d1da920ed29c470e3e0f5080a7deba077e0b8927
Felix <13115412392@163.com>
2019-09-17 10:52:18 +0800

    0

--- 复习/数据结构与算法/5_斐波那契.js
+++ 复习/数据结构与算法/5_斐波那契.js
@@ -4,6 +4,24 @@ function fib(n){
     }
     return fib(n - 2)+fib(n - 1);
 }
+// console.time('fib');
+// console.log(fib(5));//当n>43时会耗费几秒时间 时间复杂度指数级
+// console.timeEnd('fib')
+
+function fib1(n){
+    let arr = [];
+    return fibCache(arr,n);
+}
+function fibCache(arr,n){
+    if(n == 1 || n == 2){
+        return 1;
+    }
+    if(arr[n]){
+        return arr[n];
+    }
+    arr[n] = fibCache(arr,n-1)+fibCache(arr,n-2);
+    return arr[n]
+}
 console.time('fib');
-console.log(fib(5));//当n>43时会耗费几秒时间 时间复杂度指数级
+console.log(fib1(100));//缓存递归，毫秒级
 console.timeEnd('fib')
