694f627581df832db69cc3c3042e7bce1336f5d9
Felix <13115412392@163.com>
2019-09-22 15:19:55 +0800

    传参数

--- 复习/react/umi-test/src/pages/.umi/router.js
+++ 复习/react/umi-test/src/pages/.umi/router.js
@@ -13,6 +13,16 @@ const routes = [
     component: require('../index.js').default,
   },
   {
+    path: '/users/:id',
+    exact: true,
+    component: require('../users/$id.js').default,
+  },
+  {
+    path: '/users',
+    exact: true,
+    component: require('../users.js').default,
+  },
+  {
     component: () =>
       React.createElement(
         require('/usr/local/lib/node_modules/umi/node_modules/_umi-build-dev@1.12.0@umi-build-dev/lib/plugins/404/NotFound.js')
